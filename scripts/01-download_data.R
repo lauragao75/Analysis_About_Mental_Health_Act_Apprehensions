b5ce8475c65e31a8d12c7121ee9f1747c832e354
Zien Gao <lauragao75@gmail.com>
2024-09-24 19:17:27 +0000

    Added R script to download data using opendatatoronto and saved raw data

---
+++ scripts/01-download_data.R
@@ -0,0 +1,30 @@
+#### Preamble ####
+# Purpose: Simulates dataset about Mental Health Act Apprehensions
+# Author: Zien Gao
+# Date: 24 September 2024
+# Contact: lauragao75@gmail.com
+
+#### Workspace setup ####
+library(opendatatoronto)
+library(tidyverse)
+
+#### Download data ####
+# Get package information using the package name or ID
+package <- show_package("mental-health-apprehensions") 
+
+# Get all resources associated with this package
+resources <- list_package_resources("mental-health-apprehensions")
+
+# Load the data using the specific resource ID
+data <- get_resource(resource = "4a92540b-6504-4b25-827d-4511b8b9f3d9")
+
+# View the first few rows of the dataset
+print(head(data))
+
+#### Save data ####
+# Save the data to "data/raw_data/unedited_data.csv"
+write_csv(data, "data/raw_data/unedited_data.csv")
+
+
+
+         
